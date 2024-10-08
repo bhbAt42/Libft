9f4ac2d8813588ea9b257800e98af699e389c2e9
bhbAt42 <bhb@etik.com>
2024-10-08 10:04:47 +0200

    ✨ libft -> Add ft_strdup

---
+++ ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bhenriqu <bhenriqu@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 09:59:34 by bhenriqu          #+#    #+#             */
+/*   Updated: 2024/10/08 09:59:34 by bhenriqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.c>
+#include "libft.h"
+
+char *strdup(const char *s)
+{
+	size_t len;
+	char *dup;
+	size_t i;
+
+	len = ft_strlen(s);
+	dup = ft_calloc(len);
+	if (dup == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
