0d4d0133be0841722dfa91ba10897106d9fe2b09
bhbAt42 <bhb@etik.com>
2024-10-08 17:08:13 +0200

    ✨ libft -> Add ft_itoa, ft_split and ft_strmapi

---
+++ ft_itoa.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bhenriqu <bhenriqu@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 15:55:47 by bhenriqu          #+#    #+#             */
+/*   Updated: 2024/10/08 17:07:17 by bhenriqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	count_nb(int nb)
+{
+	int	len;
+
+	len = 0;
+	if (nb <= 0)
+		len++;
+	while (nb != 0)
+	{
+		nb = nb / 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	int		i;
+	char	*str;
+
+	i = count_nb(n);
+	str = malloc(sizeof(char) * (i + 1));
+	if (str == NULL)
+		return (NULL);
+	str[i--] = '\0';
+	if (n == 0)
+	{
+		str[0] = '0';
+		return (str);
+	}
+	if (n < 0)
+		str[0] = '-';
+	while (n != 0)
+	{
+		if (str[0] == '-')
+			str[i--] = '0' + -(n % 10);
+		else
+			str[i--] = '0' + (n % 10);
+		n = n / 10;
+	}
+	return (str);
+}
---
+++ ft_strmapi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bhenriqu <bhenriqu@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 16:02:52 by bhenriqu          #+#    #+#             */
+/*   Updated: 2024/10/08 17:06:53 by bhenriqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	unsigned int	len;
+	char			*str;
+
+	len = ft_strlen(s);
+	str = malloc((len + 1) * sizeof(char));
+	if (!s || !f || !str)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = f(i, (char)s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
