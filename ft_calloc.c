6e9401f67fccc08665683f332ad14e7da99f32f6
bhbAt42 <bhb@etik.com>
2024-10-08 09:56:50 +0200

    ✨ libft -> Add ft_calloc

---
+++ ft_calloc.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bhenriqu <bhenriqu@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 09:47:40 by bhenriqu          #+#    #+#             */
+/*   Updated: 2024/10/08 09:47:40 by bhenriqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.c>
+
+void	*calloc(size_t num, size_t size)
+{
+	size_t total;
+	void *ptr;
+	size_t i;
+
+	total = num * size;
+	ptr = malloc (total);
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < total)
+		*((char *)ptr + i) = 0;
+	return (ptr);
+}
